3e084d112f2e60aae340cb67864955836d2c6e58
Kent Nguyen <nguyen.dmz@gmail.com>
2017-05-14 15:54:54 +0700

    More generic seed rewards

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,11 +4,6 @@ p "Created seed users."
 Group.create(name:'Default', slack_channel: '#general', default: true)
 p "Created seed group."
 
-Reward.create(name:'Air Lounge Coupon', cost: 40, stock_count: Reward::INFINITE_STOCK, visible: true)
-Reward.create(name:'Quẩy cạn lời 500k', cost: 600, stock_count: Reward::INFINITE_STOCK, visible: true)
-Reward.create(name:'Quẩy khô máu 1000k', cost: 1100, stock_count: Reward::INFINITE_STOCK, visible: true)
-Reward.create(name:'Quẩy tẹt ga', cost: 10000, stock_count: 1, visible: true)
-Reward.create(name:'Silicon Straits Cup', cost: 100, stock_count: 15, visible: true)
-Reward.create(name:'Silicon Straits Notebook', cost: 100, stock_count: 18, visible: true)
-Reward.create(name:'T-shirt', cost: 300, stock_count: Reward::INFINITE_STOCK, visible: true)
+Reward.create(name:'Team T-shirt', cost: 180, stock_count: Reward::INFINITE_STOCK, visible: true)
+Reward.create(name:'2 Movie tickets', cost: 300, stock_count: 80, visible: true)
 p "Created seed rewards."
